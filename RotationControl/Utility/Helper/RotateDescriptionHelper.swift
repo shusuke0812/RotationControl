f13c4a6038b84855282b217f1ae4a7a245e77019
shusuke0812 <shu.ota0812@gmail.com>
2021-08-15 00:24:12 +0900

    add: 回転有無enum型のメソッドにドキュメント追加

--- RotationControl/Utility/Helper/RotateDescriptionHelper.swift
+++ RotationControl/Utility/Helper/RotateDescriptionHelper.swift
@@ -14,6 +14,10 @@ enum RotateDescriptionHelper: String {
 
 // MARK: - Config
 extension RotateDescriptionHelper {
+    /// iPhone / iPad 端末毎の回転有無を表示するための文章を生成
+    /// - Parameters:
+    ///   - iphoneRotation: iPhoneの回転有無
+    ///   - ipadRotation: iPadの回転有無
     static func setDescription(iphoneRotation: Bool, ipadRotation: Bool) -> String {
         let iphoneRotation: String = iphoneRotation ? "回転あり" : "回転なし"
         let ipadRotation: String = ipadRotation ? "回転あり" : "回転なし"
