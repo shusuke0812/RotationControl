474d3b5e217bcb200ae2b2f0b03771bdfb8371f0
shusuke0812 <shu.ota0812@gmail.com>
2024-06-24 00:04:24 +0900

    fix: SecondVCをcontainer viewに置き換え

---
+++ RotationControl/ViewController/Person/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  RotationControl
+//
+//  Created by Shusuke Ota on 2024/6/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        showMainSecondView()
+    }
+    
+    private func showMainSecondView() {
+        view.subviews.forEach { $0.removeFromSuperview() }
+        let s = UIStoryboard(name: "SecondMainViewController", bundle: nil)
+        let vc = s.instantiateInitialViewController() as! SecondMainViewController
+        addChild(vc)
+        
+        vc.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(vc.view)
+        vc.didMove(toParent: self)
+        
+        NSLayoutConstraint.activate([
+            vc.view.topAnchor.constraint(equalTo: view.topAnchor),
+            vc.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            vc.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            vc.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
